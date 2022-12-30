89e9bb3eb3935ab3274603415b60157a80d5ff34
Carlos Gomez <carlosandres0741@gmail.com>
2022-12-30 09:45:06 -0500

    feat: added cv

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,9 @@ function App () {
           <Divider color='cyan' />
           <Paragraph mt='sm' >
             Programador web
+            <Button ml='sm' variant='outline'>
+              <Anchor href='/cv-carlos-gomez.pdf'>Hoja de vida</Anchor>
+            </Button>
           </Paragraph>
         </Box>
         <img alt="Programador concentrado en el computador" className='header-img' src="/header.svg" />
