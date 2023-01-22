597965722ac4cee80e6d3141d72a72316dfae045
Carlos Gomez <carlosandres0741@gmail.com>
2023-01-22 17:12:07 -0500

    fix: remove gitignore line and format file

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,19 +4,19 @@ import { Box, Heading, Paragraph, Button, Anchor } from 'dracula-ui'
 const ProjectCard = ({ repository }) => {
   return (
     <Box className='card' color="blackSecondary" height='xs' p='md' rounded="xl" width='xs'>
-                  <Heading align="center" as="h3" color="cyanGreen" size="lg">{repository.name}</Heading>
-                  <Paragraph align='center'>{repository.description}</Paragraph>
-                  <Box className='buttons-container' display='flex'>
-                    <Button>
-                        <Anchor color='black' href={repository.html_url} target="_blank">Ver</Anchor>
-                    </Button>
-                    {
-                      repository.homepage &&
-                      <Button variant='outline'>
-                        <Anchor href={repository.homepage} target="_blank">Demo</Anchor>
-                      </Button>
-                    }
-                  </Box>
+      <Heading align="center" as="h3" color="cyanGreen" size="lg">{repository.name}</Heading>
+      <Paragraph align='center'>{repository.description}</Paragraph>
+      <Box className='buttons-container' display='flex'>
+        <Button>
+            <Anchor color='black' href={repository.html_url} target="_blank">Ver</Anchor>
+        </Button>
+        {
+          repository.homepage &&
+          <Button variant='outline'>
+            <Anchor href={repository.homepage} target="_blank">Demo</Anchor>
+          </Button>
+        }
+      </Box>
     </Box>
   )
 }
