25ed18734353e80f788c8a4d3ced1499a888fe54
Carlos Gomez <carlosandres0741@gmail.com>
2023-02-18 19:29:23 -0500

    feat: added to gitignore

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Divider, Heading } from 'dracula-ui'
 
 import ProjectCard from './ProjectCard'
+import ProjectAbout from './ProjectAbout'
 
 const Projects = ({ repositories }) => {
+  const [readme, setReadme] = useState(null)
+
   return (
     <Box as="section" className="h-screen sct-projects" width='4xl'>
           <Heading as="h2" className='title-projects' color="cyanGreen">Proyectos</Heading>
           <Divider color='purple' mb='md' />
           <Box className="container-projects" mt='md' p='md'>
             {
-              repositories.map(repository => <ProjectCard key={repository.id} repository={repository} />)
+              repositories.map(repository => <ProjectCard key={repository.id} repository={repository} setReadme={setReadme} />)
             }
           </Box>
+          {readme && <ProjectAbout readme={readme} />}
     </Box>
   )
 }
