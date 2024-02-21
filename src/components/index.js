b4d7011e2f96082c7911efafc57ad00c1b04748f
Carlos Gomez <carlosandres0741@gmail.com>
2024-02-20 23:00:00 -0500

    feat: begin astro template

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as About } from './About'
-export { default as Footer } from './Footer'
-export { default as Header } from './Header'
-export { default as ProjectCard } from './ProjectCard'
-export { default as Projects } from './Projects'
-export { default as SocialBar } from './SocialBar'
-export { default as ProjectAbout } from './ProjectAbout'
